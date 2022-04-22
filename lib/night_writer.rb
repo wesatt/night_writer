d49188e43fc9995524c8c1aa49bf7ea4bf362426
Wesley Garcia <wes@alltradestech.com>
2022-04-21 18:04:30 -0700

    Add empty file creation as output

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,7 +1,7 @@
 # require_relative "english_reader"
 # englishreader = EnglishReader.new(ARGV[0], ARGV[1])
 
-file = File.readlines(ARGV[0], chomp: true)
-file_output = ARGV[1]
-char_num = file[0].chars.count
-puts "Created '#{file_output}' containing #{char_num} characters"
+file_input = File.readlines(ARGV[0], chomp: true)
+file_output = File.open(ARGV[1], "w")
+char_num = file_input[0].chars.count
+puts "Created '#{ARGV[1]}' containing #{char_num} characters"
