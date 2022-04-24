e973d6207757deedec08f34f5da99e41cc18b8c9
Wesley Garcia <wes@alltradestech.com>
2022-04-23 17:06:50 -0700

    Add test for read_txt

--- spec/english_reader_spec.rb
+++ spec/english_reader_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe EnglishReader do
 
     expect(ereader).to be_a EnglishReader
   end
+
+  it "can read text files" do
+    ereader = EnglishReader.read_txt("./spec/fixtures/input.txt")
+
+    expect(ereader.txt_input).to eq(["hello world"])
+  end
 end
